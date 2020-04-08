405a5573a309093aedb7cffc176d4d631e080cae
rekib0023 <rkb.ra0025@gmail.com>
2020-04-08 10:45:44 +0530

    using expanded widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,38 @@ class Home extends StatelessWidget {
       centerTitle: true,
       backgroundColor: Colors.red[600],
     ),
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.center,
+    body: Row(
       children: <Widget>[
-        Row(
-          children: <Widget>[
-            Text('hello'),
-            Text(' world'),
+        Expanded(
+          flex: 3,
+          child: Image.asset('assets/beach1.jpg')
+          ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.cyan,
+            child: Text('1'),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.pinkAccent,
+            child: Text('2'),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.amber,
+            child: Text('3'),
+          ),
+        ),
           ],
         ),
-        Container(
-          padding: EdgeInsets.all(20.0),
-          color: Colors.cyan,
-          child: Text('one'),
-        ),
-        Container(
-          padding: EdgeInsets.all(30.0),
-          color: Colors.pinkAccent,
-          child: Text('two'),
-        ),
-        Container(
-          padding: EdgeInsets.all(40.0),
-          color: Colors.amber,
-          child: Text('three'),
-        ),
-      ],
-    ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
       child: Text('click'),
