00f88fd3bd6a5b3d7f49d025066dcb944518cd8f
rekib0023 <rkb.ra0025@gmail.com>
2020-04-08 08:55:52 +0530

    adding textstyle

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,16 @@ void main() => runApp(MaterialApp(
       backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('hello, world'),
+      child: Text(
+        'hello, world',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+          color: Colors.grey[600],
+          fontFamily: 'IndieFlower',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
