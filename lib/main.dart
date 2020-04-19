6bfaa7b9b60454bbe2509e3a4f9ea7edbec73c3c
rekib0023 <rkb.ra0025@gmail.com>
2020-04-19 16:30:49 +0530

    dropdown menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
@@ -11,13 +10,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List imgList = [
-    'https://images.unsplash.com/photo-1554629947-334ff61d85dc?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1534067783941-51c9c23ecefd?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1505764761634-1d77b57e1966?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    'https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjExMDk0fQ&auto=format&fit=crop&w=634&q=80',
-    'https://images.unsplash.com/photo-1542662565-7e4b66bae529?ixlib=rb-1.2.1&auto=format&fit=crop&w=564&q=80'
-  ];
+  var _numbers = [0, 1, 2, 3, 4, 5];
+  var _currentItemSelected = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -29,31 +23,23 @@ class _HomeState extends State<Home> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          CarouselSlider(
-              options: CarouselOptions(
-                initialPage: 0,
-                height: 400,
-                enlargeCenterPage: true,
-                enableInfiniteScroll: false,
-              ),
-              items: imgList.map((imgUrl) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: EdgeInsets.symmetric(horizontal: 10.0),
-                      decoration: BoxDecoration(
-                        color: Colors.green,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Image.network(
-                        imgUrl,
-                        fit: BoxFit.fill,
-                      ),
-                    );
-                  },
-                );
-              }).toList()),
+          Container(width: double.infinity),
+          Text('Select a number'),
+          DropdownButton<int>(
+            onChanged: (int newValueSelected) {
+              // print(newValueSelected);
+              setState(() {
+                _currentItemSelected = newValueSelected;
+              });
+            },
+            items: _numbers.map((int dropDownIntItem) {
+              return DropdownMenuItem<int>(
+                value: dropDownIntItem,
+                child: Text(dropDownIntItem.toString()),
+              );
+            }).toList(),
+            value: _currentItemSelected,
+          )
         ],
       ),
     );
